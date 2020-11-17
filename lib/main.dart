f0c1b573e4e91d045f679d2e8e8694d15c677280
Eoan O Dea <odeaeoan@gmail.com>
2020-11-17 14:10:52 +0100

    Cleaned up code and comments

--- lib/main.dart
+++ lib/main.dart
@@ -14,12 +14,8 @@ class _PianoAppState extends State<PianoApp> with WidgetsBindingObserver {
   @override
   initState() {
     _loadSoundFont();
-    // _initNotes();
     super.initState();
   }
-
-  /// Initialise audio cache
-  // static AudioCache player = AudioCache();
 
   void _loadSoundFont() async {
     MidiUtils.unmute();
@@ -41,46 +37,16 @@ class _PianoAppState extends State<PianoApp> with WidgetsBindingObserver {
   void didChangeAppLifecycleState(AppLifecycleState state) {
     print("State: $state");
     _loadSoundFont();
-    // _initNotes();
   }
 
-  // final List<String> notes = [
-  //   'C',
-  //   'C#',
-  //   'D',
-  //   'D#',
-  //   'E',
-  //   'E#',
-  //   'F',
-  //   'F#',
-  //   'G',
-  //   'G#',
-  //   'A',
-  //   'A#',
-  //   'B',
-  // ];
-
-  final List<int> sharps = [25, 27, 0, 30, 32, 24];
-  final List<int> notes = [24, 26, 28, 29, 31, 33, 35];
-
-  // void _initNotes() {
-  //   for (var i = 24; i < 36; i++) {
-  //     notes.add(i);
-  //   }
-
-  //   print('notes! ${notes.length}');
-  // }
-
-  // final List<int> notes = [];
+  final List<int> sharps = [25, 27, 0, 30, 32, 34, 0];
+  final List<int> notes = [24, 26, 28, 29, 31, 33, 35, 36];
 
   @override
   Widget build(BuildContext context) {
-    /// play a note depending on it's file name
-    /// expects an integer as a parameter
     void playNote(int note) {
-      // player.play('$note.mp3');
       MidiUtils.play(note);
-      //       if (feedback) {
+      //if (feedback) {
       //   VibrateUtils.light();
       // }
     }
@@ -88,18 +54,15 @@ class _PianoAppState extends State<PianoApp> with WidgetsBindingObserver {
     /// Renders a single key to the screen
     Expanded renderKey(int note, bool sharp) {
       return Expanded(
-        // child: Padding(
-        // padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
-
         child: Container(
           width: 450,
-          // margin: Margin.all,
           child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            padding: const EdgeInsets.symmetric(vertical: 5.0),
             child: FlatButton(
+              height: 10.0,
               color: sharp ? Colors.black : Colors.white,
               textColor: sharp ? Colors.white : Colors.black,
-              onPressed: () => playNote(note),
+              onPressed: () => playNote(note + (12 * 4)),
               child: Text(
                 'note $note',
               ),
@@ -110,25 +73,8 @@ class _PianoAppState extends State<PianoApp> with WidgetsBindingObserver {
     }
 
     Stack renderOctive() {
-      // List<String> sharps = [];
-      // List<String> keys = [];
-
-      // for (var i = 0; i < notes.length; i++) {
-      //   // if (notes[i].contains('#')) {
-      //     // sharps.add(notes[i]);
-      //   // } else {
-      //     // keys.add(notes[i]);
-      //   }
-      // }
-
       return Stack(
         children: [
-          // Positioned(
-          //   top: 0.0,
-          //   bottom: 0.0,
-          //   left: 0.0,
-          //   width: 500.0,
-          // child:
           Container(
             margin: EdgeInsets.all(10.0),
             child: Column(
@@ -142,44 +88,24 @@ class _PianoAppState extends State<PianoApp> with WidgetsBindingObserver {
           ),
           // ),
           Positioned(
-            // left: 0.0,
-            // right: 0.0,
-            // bottom: 100,
-            // top: 0.0,
-            // left: 0.0,
             right: 0.0,
             bottom: 0.0,
             top: 0.0,
             width: 200.0,
-
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisSize: MainAxisSize.max,
               children: <Widget>[
                 for (var i = 0; i < sharps.length; i++)
                   sharps[i] == 0
                       ? Container(
-                          width: 100 * .5,
-                          height: 100 * .5,
+                          width: 150 * .5,
+                          height: 150 * .5,
                         )
                       : renderKey(sharps[i], true)
               ],
             ),
           ),
-          // Positioned(
-          //   child: Column(
-          //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          //     crossAxisAlignment: CrossAxisAlignment.stretch,
-          //     children: [
-          //       for (var i = 0; i < sharps.length; i++)
-          //         renderKey(sharps[i], sharp: true),
-          //     ],
-          //   ),
-          // right: 00.0,
-          // bottom: 50.0,
-          // top: 50.0,
-          // width: 200.0,
-          // ),
         ],
       );
     }
--- lib/notes.dart
+++
@@ -1,7 +0,0 @@
-const List notes = ['A', 'A#'];
-
-class Note {
-  String name;
-
-  Note(this.name);
-}
